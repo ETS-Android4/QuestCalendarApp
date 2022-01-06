da0a12fe7ccb7f0b7fdefad71bce13dcbb2307ef
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2022-01-06 14:50:21 +0100

    Created daily quest button

--- QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/CalendarFragment.java
+++ QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/CalendarFragment.java
@@ -27,15 +27,7 @@ public class CalendarFragment extends Fragment {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        */
+
         return root;
     }
 
