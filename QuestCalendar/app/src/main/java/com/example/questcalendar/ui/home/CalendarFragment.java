50277434168651bd35f4318fd807f210f20513b8
Jaja <jade.neav@gmail.com>
2022-01-07 10:32:53 +0100

    daily and monthly views buttons in calendar fragment

--- QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/CalendarFragment.java
+++ QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/CalendarFragment.java
@@ -23,6 +23,12 @@ import com.example.questcalendar.databinding.FragmentCalendarBinding;
 
 public class CalendarFragment extends Fragment {
 
+    public final static int MONTHLY_VIEW_STATE = 0;
+    public final static int DAILY_VIEW_STATE = 1;
+
+    private int viewState;
+    private Button monthly;
+    private Button daily;
     private CalendarViewModel homeViewModel;
     private FragmentCalendarBinding binding;
     Button dailyQuest;
@@ -34,6 +40,58 @@ public class CalendarFragment extends Fragment {
 
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //Display the monthly view
+        viewState = MONTHLY_VIEW_STATE;
+        //to use a fragment in a fragment
+        getChildFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.frequency_view_fragment, MonthlyViewFragment.class, null)
+                .commit();
+
+        //Go to daily view
+
+        daily = (Button) root.findViewById(R.id.daily_view_button);
+        daily.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                if (viewState==MONTHLY_VIEW_STATE) {
+
+                    //if (savedInstanceState == null) {
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("day_of_month", 6);
+                    bundle.putInt("month", 1);
+                    bundle.putInt("year", 2022);
+                    bundle.putChar("day_of_weak", 'T');
+
+                    getChildFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.frequency_view_fragment, DailyViewFragment.class, bundle)
+                            .commit();
+                    //}
+                }
+            }
+        });
+
+        monthly = (Button) root.findViewById(R.id.monthly_view_button);
+        monthly.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+
+
+                    getChildFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.frequency_view_fragment, MonthlyViewFragment.class, null)
+                            .commit();
+
+
+            }
+        });
+
 
         //Go to daily quest
         dailyQuest = (Button) root.findViewById(R.id.daily_quest_button);
@@ -58,31 +116,14 @@ public class CalendarFragment extends Fragment {
 
         /*
         //Monthly
-        //to use a fragment in a fragment
-        getChildFragmentManager().beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.frequency_view_fragment, MonthlyViewFragment.class, null)
-                .commit();
 
-        return root;
 
          */
 
 
         //Daily
         //to use a fragment in a fragment
-        if (savedInstanceState == null) {
-            Bundle bundle = new Bundle();
-            bundle.putInt("day_of_month",6);
-            bundle.putInt("month",1);
-            bundle.putInt("year",2022);
-            bundle.putChar("day_of_weak",'T');
 
-            getChildFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.frequency_view_fragment, DailyViewFragment.class, bundle)
-                    .commit();
-        }
 
         return root;
 
