a92c65d06842f780730d90eaa9928aca4549c222
Jaja <jade.neav@gmail.com>
2022-01-18 20:29:35 +0100

    added task to the monthly view but not working yet

--- QuestCalendar/app/src/main/java/com/example/questcalendar/ui/dashboard/TaskManager.java
+++ QuestCalendar/app/src/main/java/com/example/questcalendar/ui/dashboard/TaskManager.java
@@ -1,6 +1,7 @@
 package com.example.questcalendar.ui.dashboard;
 
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,8 @@ import com.example.questcalendar.MainActivity;
 import com.example.questcalendar.UserHelperClass;
 import com.example.questcalendar.calendar.Date;
 import com.example.questcalendar.calendar.Task;
+import com.example.questcalendar.calendar.TaskComparator;
+import com.example.questcalendar.calendar.exceptions.MyException;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +20,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -38,7 +42,7 @@ public class TaskManager {
     private List<Task> punctual;
 
     //the list of the current day tasks
-    private List<Task> tasksOfTheDay;
+    private ArrayList<Task> tasksOfTheDay;
 
     //the way to find the user
     //username for now
@@ -53,18 +57,141 @@ public class TaskManager {
     //to go to the tasks database
     private final static String TASKS = "task";
 
+    //to get the frequency of a task
+    private final static String FREQUENCY = "frequency";
+
+    //to get the id of a task
+    private final static String ID = "id";
+
+    //to get the title of a task
+    private final static String TITLE = "title";
+
+    //to get the hour of a task
+    private final static String HOUR = "hour";
+
+    //to get the description of a task
+    private final static String DESCRIPTION = "description";
+
+    //to get the day of a task
+    private final static String DAY = "day";
+
+    //to get the month of a task
+    private final static String MONTH = "month";
+
+    //to get the year of a task
+    private final static String YEAR = "year";
+
+    //when the description is empty
+    private final static String DEFAULT_DESCRIPTION = "";
+
     //reference to the database
     DatabaseReference reference;
 
-    TaskManager(Date day) {
+    public TaskManager(Date day) {
         //set the date
         this.currentDay = day;
         this.daily = new ArrayList<Task>();
         this.monthly = new ArrayList<Task>();
         this.punctual = new ArrayList<Task>();
         this.tasksOfTheDay = new ArrayList<Task>();
-        this.maxTaskId = 1;
-        this.reference = FirebaseDatabase.getInstance(QUEST_CALENDAR_LINK).getReference(USERS);
+        this.maxTaskId = 0;
+        this.reference = FirebaseDatabase.getInstance(QUEST_CALENDAR_LINK).getReference(USERS).child(CURRENT_USERNAME).child(TASKS);
+
+
+        //get the tasks from the database
+
+        this.reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                        //building the task from the database
+
+                            //getting the frequency
+                        int frequency = Integer.parseInt(child.child(FREQUENCY).getValue(String.class));
+
+
+                            //getting the ID
+                        int id = Integer.parseInt(child.child(ID).getValue(String.class));
+
+
+                            //getting the title
+                        String title = child.child(TITLE).getValue(String.class);
+
+
+                            //getting the hour
+                        int hour = Integer.parseInt(child.child(HOUR).getValue(String.class));
+
+
+                            //getting the description
+                            //description can be null
+                        String description;
+                        if (child.child(DESCRIPTION).exists()) {
+                            description = child.child(DESCRIPTION).getValue(String.class);
+                        } else {
+                            description = DEFAULT_DESCRIPTION;
+                        }
+
+                            //getting the date
+                        int day = Integer.parseInt(child.child(DAY).getValue(String.class));
+                        int month = Integer.parseInt(child.child(MONTH).getValue(String.class));
+                        int year = Integer.parseInt(child.child(YEAR).getValue(String.class));
+                        Date date = new Date(day, month, year, 0);
+
+                        Task currentTask = new Task(id, title, description, date, hour, frequency);
+
+                        //adding the task in the right list
+                        if (frequency == Task.DAILY) {
+                            daily.add(currentTask);
+                        } else if (frequency == Task.MONTHLY) {
+                            monthly.add(currentTask);
+                        } else {
+                            punctual.add(currentTask);
+                        }
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
+        //finding the task of the current day
+
+        for (Task d : daily) {
+            tasksOfTheDay.add(d);
+
+            if (d.getId() > this.maxTaskId) {
+                this.maxTaskId = d.getId();
+            }
+        }
+
+        for (Task m : monthly) {
+            if (m.getDay().getDayOfMonth() == currentDay.getDayOfMonth()) {
+                tasksOfTheDay.add(m);
+            }
+
+            if (m.getId() > this.maxTaskId) {
+                this.maxTaskId = m.getId();
+            }
+        }
+
+        for (Task p : punctual) {
+            if (p.getDay().isEqual(currentDay)) {
+                tasksOfTheDay.add(p);
+            }
+
+            if (p.getId() > this.maxTaskId) {
+                this.maxTaskId = p.getId();
+            }
+        }
+
 
 
     }
@@ -83,14 +210,14 @@ public class TaskManager {
 
 
             //find the id
-        this.maxTaskId++;
+        this.maxTaskId = this.maxTaskId +1;
         String idNewTask = Integer.toString(this.maxTaskId);
 
         TaskHelper newTask = new TaskHelper(idNewTask, titleNewTask, descriptionNewTask, hourNewTask, frequencyNewTask, dayNewTask, monthNewTask, yearNewTask);
 
 
         //gets the username as a identifier
-        reference.child(CURRENT_USERNAME).child(TASKS).child(idNewTask).setValue(newTask);
+        reference.child(idNewTask).setValue(newTask);
 
 
     }
@@ -101,13 +228,47 @@ public class TaskManager {
     }
 
     //return the sorted list by hour of the tasks of the currentDay
-    public PriorityQueue<Task> getTaskOfTheDay() {
-        return new PriorityQueue<Task>(this.tasksOfTheDay); //add a comparator for task
+    public ArrayList<Task> getTaskOfTheDay() {
+        TaskComparator c = new TaskComparator();
+        Collections.sort(this.tasksOfTheDay, c); //add a comparator for task
+        return this.tasksOfTheDay;
     }
 
     public void setDay(Date newDay) {
         this.currentDay = newDay;
+        this.tasksOfTheDay = new ArrayList<Task>();
+
+        //finding the task of the current day
+        for (Task d : daily) {
+            tasksOfTheDay.add(d);
+
+            if (d.getId() > this.maxTaskId) {
+                this.maxTaskId = d.getId();
+            }
+        }
+
+        for (Task m : monthly) {
+            if (m.getDay().getDayOfMonth() == currentDay.getDayOfMonth()) {
+                tasksOfTheDay.add(m);
+            }
+
+            if (m.getId() > this.maxTaskId) {
+                this.maxTaskId = m.getId();
+            }
+        }
+
+        for (Task p : punctual) {
+            if (p.getDay().isEqual(currentDay)) {
+                tasksOfTheDay.add(p);
+            }
+
+            if (p.getId() > this.maxTaskId) {
+                this.maxTaskId = p.getId();
+            }
+        }
     }
+
+
 
 
 }
--- QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/MonthlyViewFragment.java
+++ QuestCalendar/app/src/main/java/com/example/questcalendar/ui/home/MonthlyViewFragment.java
@@ -19,6 +19,7 @@ import com.example.questcalendar.R;
 import com.example.questcalendar.calendar.Date;
 import com.example.questcalendar.calendar.Task;
 import com.example.questcalendar.calendar.exceptions.MyException;
+import com.example.questcalendar.ui.dashboard.TaskManager;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -37,6 +38,7 @@ public class MonthlyViewFragment extends Fragment {
     private int taskNb;
     private ArrayList<Task> tasks;
     private Date selectedDate;
+    private TaskManager taskManager;
 
 
 
@@ -70,10 +72,12 @@ public class MonthlyViewFragment extends Fragment {
                 //selectedDay.setText(date);
                 selectedDate = new Date(dayOfMonth, month +1, year, (dayOfWeek -1));
                 //debug.setText(date + " " + selectedDate.toString());
+                taskManager.setDay(selectedDate);
                 displayTaskNb(view);
             }
         });
 
+        taskManager = new TaskManager(selectedDate);
         displayTaskNb(view);
 
         return view;
@@ -81,28 +85,9 @@ public class MonthlyViewFragment extends Fragment {
 
     private void displayTaskNb(View view) {
 
-        taskNb = 0;
-        tasks = new ArrayList<Task>();
-        try {
-            Date date1 = new Date(7, 1, 2022, 4);
-            Date date2 = new Date(8, 1, 2022, 6);
-            Date date3 = new Date(5, 1, 2022, 3);
-            Task t1 = new Task(1, "dentist appointment", "my tooth is really hurting me", date1, 8, 0);
-            Task t2 = new Task(0, "Mobile Computing exam", "studying for the exam", date2, 10, 0);
-            Task t3 = new Task(2, "fork my process", "I love SEC", date2, 10, 0);
-            tasks.add(t1);
-            tasks.add(t2);
-            tasks.add(t3);
-        } catch (MyException e) {
-            Toast.makeText(getContext() , e.getMessage(), Toast.LENGTH_LONG).show();
-        }
+        ArrayList<Task> tasksOfTheDay = taskManager.getTaskOfTheDay();
+        taskNb = tasksOfTheDay.size();
 
-
-        for (Task t : tasks) {
-            if (this.selectedDate.isEqual(t.getDay())) {
-                taskNb++;
-            }
-        }
 
         taskNbView.setText("Number of tasks for today: " + taskNb);
     }
